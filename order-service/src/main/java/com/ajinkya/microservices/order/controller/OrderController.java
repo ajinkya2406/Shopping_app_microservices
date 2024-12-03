dbd7163841d09e0a00761f8caeece002a2b71281
ajju06 <pewdikai.gamer@gmail.com>
2024-12-03 20:12:34 +0530

    order service fully commit and tested

--- order-service/src/main/java/com/ajinkya/microservices/order/controller/OrderController.java
+++ order-service/src/main/java/com/ajinkya/microservices/order/controller/OrderController.java
@@ -1,4 +1,24 @@
 package com.ajinkya.microservices.order.controller;
 
+
+import com.ajinkya.microservices.order.dto.OrderRequest;
+import com.ajinkya.microservices.order.service.Orderservice;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+
 public class OrderController {
+
+    private final Orderservice orderservice;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderservice.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
 }
