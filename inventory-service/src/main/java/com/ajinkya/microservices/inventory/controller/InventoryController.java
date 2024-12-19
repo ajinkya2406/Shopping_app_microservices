ef00b47b0d601f2d869f473ef39d123025bfeac2
ajju06 <pewdikai.gamer@gmail.com>
2024-12-19 17:02:32 +0530

    inventory service , sql initiated and docker initiated

---
+++ inventory-service/src/main/java/com/ajinkya/microservices/inventory/controller/InventoryController.java
@@ -0,0 +1,23 @@
+package com.ajinkya.microservices.inventory.controller;
+
+
+import com.ajinkya.microservices.inventory.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    @Autowired
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+}
